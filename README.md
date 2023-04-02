3214f16dd9123dc5b1ca6f7c30c75e934deede90
Bow <33423299+StreetTT@users.noreply.github.com>
2023-04-02 18:37:20 +0100

    v1 - simple program

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # MoneyMoves
-A Notion Integrated Finance System
+A notions integrated application to manage money flowing through multiple account and display the information. 
+
+## Set Up
+
+1. Set Up Notion
+	
+	1. Create a [Notion Account](https://www.notion.so/product)
+  	
+	2. Create a copy of the [Landing Page](https://valiant-silica-d27.notion.site/MoneyMoves-Template-a2e628aecc714c52802ddd8a572cbcee) in your notion
+
+1. Create Notion integration
+  	
+	3. Go to the [Integrations](https://www.notion.so/my-integrations) page in your Notion account.
+  	
+	4. Click the "New integration" button.
+  	
+	5. Give your integration a name and description.
+  	
+	6. Select "Private integration" as the integration type.
+ 	
+	7. Click "Create integration".
+
+3. Install Aplication
+  	
+	1. Clone the repository or download the program as a zip file and unzip it. 
+  	
+	2. In the root directory of the program, edit `.env` and change the following environment variables: 
+  	
+	`notiontoken`: Your Internal Integration Token, hiden in the new integration just created  
+  
+  	![image](https://user-images.githubusercontent.com/33423299/209343663-8be6a295-af39-45db-a4fe-bbbf1a21d404.png)
+  
+  	`landingurl`: The URL of the coppied page
+  
+  	![image](#)
---
+++ main.py
@@ -0,0 +1,120 @@
+from os import getenv
+from requests import request
+from sys import exit
+from json import loads
+
+
+def URLToDBID(url):
+    parts = url.split("/")
+    if len(parts) >= 2:
+        return parts[-1].split("-")[-1].split("?")[0]
+    else:
+        print("No URL Detected")
+        exit()
+
+
+def MakeRequest(method, url, message, data=None):
+    if data is None:
+        res = request(method=method, url=url, headers=HEADERS)
+    else:
+        res = request(method=method, url=url, json=data, headers=HEADERS)
+    print(f"{res.status_code} | {method} | {message}\n")
+    if res.status_code != 200:
+        print(f"{res.text}\n")
+    else:
+        return loads(res.text)
+
+
+LANDINGID = URLToDBID(getenv("landingurl"))
+TYPE = ["Income", "Expense"]
+NOTIONTOKEN = getenv("notiontoken")
+HEADERS = {
+    "Authorization": f"Bearer {NOTIONTOKEN}",
+    "Notion-Version": "2022-06-28",
+    "Content-Type": "application/json",
+}
+LandingPageChildren = MakeRequest(
+    "GET",
+    f"https://api.notion.com/v1/blocks/{LANDINGID}/children",
+    "Landing Page Children",
+)["results"]
+for block in LandingPageChildren:
+    if block.get("child_database") == None:
+        SettingsPageID = block["id"]
+    else:
+        if (block.get("child_database")).get("title") == "Transactions":
+            TransactionsDBID = block["id"]
+SettingsPageChildren = MakeRequest(
+    "GET",
+    f"https://api.notion.com/v1/blocks/{SettingsPageID}/children",
+    "Settings Page Children",
+)["results"]
+for block in SettingsPageChildren:
+    if block.get("type", None) != None:
+        AccountDBID = block["id"]
+AccountDatabaseData = MakeRequest(
+    "POST", f"https://api.notion.com/v1/databases/{AccountDBID}/query", "Accounts"
+)["results"]
+Accounts = {}
+for record in AccountDatabaseData:
+    Accounts.update(
+        {record["properties"]["Name"]["title"][0]["plain_text"]: record["id"]}
+    )
+
+account = -1
+while account == -1:
+    print("What account is this transaction on?")
+    for index, transaction in enumerate(Accounts.keys()):
+        print(str(index + 1) + ") " + transaction)
+    account = input()
+    try:
+        account = int(account)
+        if account not in list(range(1, len(Accounts.keys()) + 1)):
+            account = -1
+            print("Pick a valid choice")
+    except ValueError:
+        account = -1
+        print("Pick a valid choice")
+account = (list(Accounts.keys()))[account - 1]
+expenseType = -1
+while expenseType == -1:
+    print("What type of transaction is it?")
+    for index, type in enumerate(TYPE):
+        print(str(index + 1) + ") " + type)
+    expenseType = input()
+    try:
+        expenseType = int(expenseType)
+        if expenseType not in list(range(1, len(TYPE) + 1)):
+            expenseType = -1
+            print("Pick a valid choice")
+    except ValueError:
+        expenseType = -1
+        print("Pick a valid choice")
+expenseType = TYPE[expenseType - 1]
+amount = -1
+while amount < 0:
+    print("How much was this transaction?")
+    amount = input()
+    try:
+        amount = int(amount)
+        if amount < 0:
+            print("Transaction must be more than 0")
+    except ValueError:
+        amount = -1
+        print("Pick a valid choice")
+if expenseType == "Expense":
+    amount = -amount
+print("Sumarise this transaction")
+reason = input()
+data = {
+    "parent": {"type": "database_id", "database_id": TransactionsDBID},
+    "properties": {
+        "Account": {"type": "relation", "relation": [{"id": Accounts[account]}]},
+        "Amount": {"type": "number", "number": amount},
+        "Name": {
+            "type": "title",
+            "title": [{"type": "text", "text": {"content": reason}}],
+        },
+    },
+}
+MakeRequest("POST", "https://api.notion.com/v1/pages", "New Transaction", data)
