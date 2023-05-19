181713f6fa7fb20af9d6413b46fa8774d9df9d9b
Bow@mercury <streettt@noreply.github.com>
2023-05-19 19:40:25 +0100

    Version 2 - OOP + Tunnels and Round Ups

--- README.md
+++ README.md
@@ -36,3 +36,20 @@ A notions integrated application to manage money flowing through multiple accoun
   	![image](https://user-images.githubusercontent.com/33423299/229369429-2cdcb09c-8b21-4878-a365-9d0ed8074cff.png)
    	
 	3. Install the required dependencies by running `pip install -r requirements.txt.` in the root directory of the program
+
+## Settings
+
+Name: 
+- Name of the Account
+
+Round Ups:
+- This rounds up all expenses to the nearest pound and deposits the diffrence into the account linked. 
+- The linked account should be entered into the `Round Up To` column
+- Leave the `Round Up To` column empty to disable the feature on the account.
+
+Tunnels:
+- This connects to accounts together, so that expenses/incomes from one account are payed in to/taken from the linked account.
+- To determine if this happens when money is payed in/out of the account, select a drop down option in the `Tunnel When` column
+- The linked account the money is payed in/out to should be entered into the `Tunnel To` column
+- Leave either the `Tunnel When` column or the `Tunnel To` column to disable the feature for the account
+
--- main.py
+++ main.py
@@ -5,15 +5,6 @@ from json import loads
 from datetime import datetime as dt
 
 
-def URLToDBID(url):
-    parts = url.split("/")
-    if len(parts) >= 2:
-        return parts[-1].split("-")[-1].split("?")[0]
-    else:
-        print("No URL Detected")
-        exit()
-
-
 def MakeRequest(method, url, message, data=None):
     if data is None:
         res = request(method=method, url=url, headers=HEADERS)
@@ -26,97 +17,297 @@ def MakeRequest(method, url, message, data=None):
         return loads(res.text)
 
 
-LANDINGID = URLToDBID(getenv("landingurl"))
 TYPE = ["Income", "Expense"]
 NOTIONTOKEN = getenv("notiontoken")
 HEADERS = {
     "Authorization": f"Bearer {NOTIONTOKEN}",
     "Notion-Version": "2022-06-28",
-    "Content-Type": "application/json",
+    "Content-Type": "application/json"
 }
-LandingPageChildren = MakeRequest(
-    "GET",
-    f"https://api.notion.com/v1/blocks/{LANDINGID}/children",
-    "Landing Page Children",
-)["results"]
-for block in LandingPageChildren:
-    if block.get("child_database") == None:
-        SettingsPageID = block["id"]
-    else:
-        if (block.get("child_database")).get("title") == "Transactions":
-            TransactionsDBID = block["id"]
-SettingsPageChildren = MakeRequest(
-    "GET",
-    f"https://api.notion.com/v1/blocks/{SettingsPageID}/children",
-    "Settings Page Children",
-)["results"]
-for block in SettingsPageChildren:
-    if block.get("type", None) != None:
-        AccountDBID = block["id"]
-AccountDatabaseData = MakeRequest(
-    "POST", f"https://api.notion.com/v1/databases/{AccountDBID}/query", "Accounts"
-)["results"]
-Accounts = {}
-for record in AccountDatabaseData:
-    Accounts.update(
-        {record["properties"]["Name"]["title"][0]["plain_text"]: record["id"]}
-    )
 
-account = -1
-while account == -1:
-    print("What account is this transaction on?")
-    for index, transaction in enumerate(Accounts.keys()):
-        print(str(index + 1) + ") " + transaction)
-    account = input()
-    try:
-        account = int(account)
-        if account not in list(range(1, len(Accounts.keys()) + 1)):
-            account = -1
-            print("Pick a valid choice")
-    except ValueError:
-        account = -1
-        print("Pick a valid choice")
-account = (list(Accounts.keys()))[account - 1]
-expenseType = -1
-while expenseType == -1:
-    print("What type of transaction is it?")
-    for index, type in enumerate(TYPE):
-        print(str(index + 1) + ") " + type)
-    expenseType = input()
-    try:
-        expenseType = int(expenseType)
-        if expenseType not in list(range(1, len(TYPE) + 1)):
-            expenseType = -1
-            print("Pick a valid choice")
-    except ValueError:
-        expenseType = -1
-        print("Pick a valid choice")
-expenseType = TYPE[expenseType - 1]
-amount = -1
-while amount < 0:
-    print("How much was this transaction?")
-    amount = input()
-    try:
-        amount = float(amount)
-        if amount < 0:
-            print("Transaction must be more than 0")
-    except ValueError:
-        amount = -1
-        print("Pick a valid choice")
-if expenseType == "Expense":
-    amount = -amount
-print("Sumarise this transaction")
-reason = input()
-data = {
-    "parent": {"type": "database_id", "database_id": TransactionsDBID},
-    "properties": {
-        "Account": {"type": "relation", "relation": [{"id": Accounts[account]}]},
-        "Amount": {"type": "number", "number": amount},
-        'Date': {'type': 'date', "date": {"start": (dt.now()).isoformat()}},
-        "Name": {
-            "type": "title",
-            "title": [{"type": "text", "text": {"content": reason}}],
-        },
-    },
-}
-MakeRequest("POST", "https://api.notion.com/v1/pages", "New Transaction", data)
+
+class MoneyMove:
+
+    def __init__(self, url: str):
+        self.__Accounts = []
+        parts = url.split('/')
+        if len(parts) >= 2:
+            self.__URL = parts[-1].split('-')[-1].split('?')[0]
+        else:
+            print("No URL Entered")
+            exit()
+        LandingPageChildren = MakeRequest(
+            "GET", f"https://api.notion.com/v1/blocks/{self.__URL}/children",
+            "Landing Page Children")["results"]
+        for block in LandingPageChildren:
+            if block.get("child_database") == None:
+                SettingsPageID = block["id"]
+            else:
+                if (block.get("child_database")
+                    ).get("title") == "Transactions":
+                    self.__TransactionsDBID = block["id"]
+        SettingsPageChildren = MakeRequest(
+            "GET",
+            f"https://api.notion.com/v1/blocks/{SettingsPageID}/children",
+            "Settings Page Children")["results"]
+        for block in SettingsPageChildren:
+            if block.get("type", None) != None:
+                AccountDBID = block["id"]
+        AccountDatabaseData = MakeRequest(
+            "POST", f"https://api.notion.com/v1/databases/{AccountDBID}/query",
+            "Accounts")["results"]
+        for record in AccountDatabaseData:
+            roundUp = record["properties"]["Round Up To"]["relation"]
+            if len(roundUp) == 1:
+                roundUp = roundUp[0]["id"]
+            else:
+                roundUp = False
+            when = record["properties"]["Tunnel When"]["select"]
+            if when == None:
+                when = False
+            else:
+                when = when["name"]
+            to = record["properties"]["Tunnel To"]["relation"]
+            if len(to) == 1:
+                to = to[0]["id"]
+            else:
+                to = False
+            self.AppendAccount(
+                Account(record["properties"]["Name"]["title"][0]["plain_text"],
+                        record["id"], roundUp, when, to))
+
+    def GetURL(self):
+        return self.__URL
+
+    def GetTransactionsDBID(self):
+        return self.__TransactionsDBID
+
+    def GetAccount(self, index=-1):
+        if index != -1:
+            return self.__Accounts[index]
+        return self.__Accounts
+
+    def AppendAccount(self, account):
+        self.__Accounts.append(account)
+
+
+class Account:
+
+    def __init__(self,
+                 name: str,
+                 iD: str,
+                 roundUp: bool = False,
+                 when: str = False,
+                 to: str = False):
+        self.__Name = name
+        self.__ID = iD
+        self.__RoundUp = roundUp
+        if not (when or to):
+            self.__Tunnel = False
+        else:
+            self.__Tunnel = {"When": when, "To": to}
+
+    def GetName(self):
+        return self.__Name
+
+    def GetID(self):
+        return self.__ID
+
+    def GetRoundUp(self):
+        return self.__RoundUp
+
+    def GetTunnel(self):
+        return self.__Tunnel
+
+
+class Transaction:
+
+    def __init__(self, mm: MoneyMove):
+        self.__mm = mm
+        self.Account = -1
+        self.GetAccount()
+        self.ExpenseType = -1
+        self.GetExpenseType()
+        self.Amount = -1
+        self.GetAmount()
+        self.Reason = None
+        self.GetReason()
+        self.__MakeTransaction()
+
+    def __MakeTransaction(self):
+        amount = self.Amount
+        if self.Account.GetRoundUp() and self.ExpenseType == "Expense":
+            amount = (int(abs(self.Amount)) + 1)
+        data = {
+            'parent': {
+                'type': 'database_id',
+                'database_id': self.__mm.GetTransactionsDBID()
+            },
+            'properties': {
+                'Account': {
+                    'type': 'relation',
+                    'relation': [{
+                        'id': self.Account.GetID()
+                    }]
+                },
+                'Amount': {
+                    'type': 'number',
+                    'number': amount
+                },
+                'Date': {
+                    'type': 'date',
+                    "date": {
+                        "start": (dt.now()).isoformat()
+                    }
+                },
+                'Name': {
+                    'type': 'title',
+                    'title': [{
+                        'type': 'text',
+                        'text': {
+                            'content': self.Reason
+                        }
+                    }]
+                }
+            }
+        }
+        MakeRequest("POST", "https://api.notion.com/v1/pages",
+                    "New Transaction", data)
+        if self.Account.GetRoundUp() and self.ExpenseType == "Expense":
+            self.__MakeRoundUpTransaction()
+        if self.Account.GetTunnel(
+        ) and self.ExpenseType == self.Account.GetTunnel()["When"]:
+            self.__MakeTunnelTransaction()
+
+    def __MakeRoundUpTransaction(self):
+        data = {
+            'parent': {
+                'type': 'database_id',
+                'database_id': self.__mm.GetTransactionsDBID()
+            },
+            'properties': {
+                'Account': {
+                    'type': 'relation',
+                    'relation': [{
+                        'id': self.Account.GetRoundUp()
+                    }]
+                },
+                'Amount': {
+                    'type': 'number',
+                    'number': (int(abs(self.Amount)) + 1) - abs(self.Amount)
+                },
+                'Date': {
+                    'type': 'date',
+                    "date": {
+                        "start": (dt.now()).isoformat()
+                    }
+                },
+                'Name': {
+                    'type': 'title',
+                    'title': [{
+                        'type': 'text',
+                        'text': {
+                            'content': self.Reason
+                        }
+                    }]
+                }
+            }
+        }
+        MakeRequest("POST", "https://api.notion.com/v1/pages",
+                    "Round Up Transaction", data)
+
+    def __MakeTunnelTransaction(self):
+        data = {
+            'parent': {
+                'type': 'database_id',
+                'database_id': self.__mm.GetTransactionsDBID()
+            },
+            'properties': {
+                'Account': {
+                    'type': 'relation',
+                    'relation': [{
+                        'id': self.Account.GetTunnel()["To"]
+                    }]
+                },
+                'Amount': {
+                    'type': 'number',
+                    'number': -self.Amount
+                },
+                'Date': {
+                    'type': 'date',
+                    "date": {
+                        "start": (dt.now()).isoformat()
+                    }
+                },
+                'Name': {
+                    'type': 'title',
+                    'title': [{
+                        'type': 'text',
+                        'text': {
+                            'content': self.Reason
+                        }
+                    }]
+                }
+            }
+        }
+        MakeRequest("POST", "https://api.notion.com/v1/pages",
+                    "Tunnel Transaction", data)
+
+    def GetAccount(self):
+        while self.Account == -1:
+            print("What account is this transaction on?")
+            for index, account in enumerate(self.__mm.GetAccount()):
+                print(str(index + 1) + ") " + account.GetName())
+            self.Account = input()
+            try:
+                self.Account = int(self.Account)
+                if self.Account not in list(
+                        range(1,
+                              len(self.__mm.GetAccount()) + 1)):
+                    self.Account = -1
+                    print("Pick a valid choice")
+            except ValueError:
+                self.Account = -1
+                print("Pick a valid choice")
+        self.Account = self.__mm.GetAccount(self.Account - 1)
+
+    def GetExpenseType(self):
+        while self.ExpenseType == -1:
+            print("What type of transaction is it?")
+            for index, type in enumerate(TYPE):
+                print(str(index + 1) + ") " + type)
+            self.ExpenseType = input()
+            try:
+                self.ExpenseType = int(self.ExpenseType)
+                if self.ExpenseType not in list(range(1, len(TYPE) + 1)):
+                    self.ExpenseType = -1
+                    print("Pick a valid choice")
+            except ValueError:
+                self.ExpenseType = -1
+                print("Pick a valid choice")
+        self.ExpenseType = TYPE[self.ExpenseType - 1]
+
+    def GetAmount(self):
+        while self.Amount < 0:
+            print("How much was this transaction?")
+            self.Amount = input()
+            try:
+                self.Amount = float(self.Amount)
+                if self.Amount < 0:
+                    print("Transaction must be more than 0")
+            except ValueError:
+                self.Amount = -1
+                print("Pick a valid choice")
+        if self.ExpenseType == "Expense":
+            self.Amount = -self.Amount
+
+    def GetReason(self):
+        print("Sumarise this transaction")
+        self.Reason = input()
+
+
+CurrentMM = MoneyMove(getenv("landingurl"))
+again = "Y"
+while again == "Y":
+    Transaction(CurrentMM)
+    again = input("Make another Transaction (Y/N): ").upper()
